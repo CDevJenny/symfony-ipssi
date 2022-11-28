ee72aa5c18e58d3d5baa2b2ecd234767585b61e3
CJenny <c.dev.jenny@gmail.com>
2022-11-28 15:48:50 +0100

    Add User/Register/Login and permissions to route Article

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/')]
 class HomeController extends AbstractController
 {
-    public function __construct(protected ManagerRegistry $registery) 
+    public function __construct(
+        protected ManagerRegistry $registery, 
+        protected UserPasswordHasherInterface $encoder) 
     {
     }
 
@@ -26,4 +31,21 @@ class HomeController extends AbstractController
             'article' => $article,
         ]);
     }
+/*     #[Route('/create_user', name:'app_create_user')]
+    public function createUser() {
+        $user = new User;
+        
+        $user->setEmail('test@test.com');
+        $user->setRoles(['ROLE_USER']);
+        $plainPassword = 'mdp1234';
+        $password = $this->encoder->hashPassword($user, $plainPassword);
+        $user->setPassword($password);
+        
+        $entityManager = $this->registery->getManager();
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+    } */
 }
