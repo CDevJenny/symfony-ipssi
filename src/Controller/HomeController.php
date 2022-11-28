1163fbba5d7e6b494873cab9d4942ec291e6e73e
CJenny <c.dev.jenny@gmail.com>
2022-11-28 17:28:35 +0100

    Fix index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends AbstractController
         protected UserPasswordHasherInterface $encoder) 
     {
     }
-    
+
     #[Route('', name: 'app_home')]
     public function index(): Response
     {
