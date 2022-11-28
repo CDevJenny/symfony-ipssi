168201367301b417fae4ad87edd8bf3442ae7de2
CJenny <c.dev.jenny@gmail.com>
2022-11-28 17:27:33 +0100

    Add access control to /profile and remove useless callbacks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,33 +19,10 @@ class HomeController extends AbstractController
         protected UserPasswordHasherInterface $encoder) 
     {
     }
-
+    
     #[Route('', name: 'app_home')]
     public function index(): Response
     {
-        $articleRepository = $this->registery->getRepository(Article::class);
-
-        $article = $articleRepository->find(1);
-
-        return $this->render('home/index.html.twig', [
-            'article' => $article,
-        ]);
+        return $this->render('home/index.html.twig');
     }
-/*     #[Route('/create_user', name:'app_create_user')]
-    public function createUser() {
-        $user = new User;
-        
-        $user->setEmail('test@test.com');
-        $user->setRoles(['ROLE_USER']);
-        $plainPassword = 'mdp1234';
-        $password = $this->encoder->hashPassword($user, $plainPassword);
-        $user->setPassword($password);
-        
-        $entityManager = $this->registery->getManager();
-
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        return $this->redirectToRoute('app_home');
-    } */
 }
