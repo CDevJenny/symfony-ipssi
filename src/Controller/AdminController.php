987bfdebba0e6c650393cdf3b5a9fcad9b9e55c5
CJenny <c.dev.jenny@gmail.com>
2022-11-29 17:29:33 +0100

    Add admin controller and route

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    public function __construct(protected AuthorizationCheckerInterface $authorizationChecker)
+    {
+    }
+    #[Route('', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
